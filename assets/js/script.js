80c4043d3246a5f3a737be1c483c50c4ac4db358
Allan Espitia <espitiaallan@gmail.com>
2023-08-04 12:13:51 -0600

    mejorando a patricio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
-console.log("Págiona en construccion con Patricio estrella");
+console.log("Página en construcción con Patricio Estrella");
 
-function clickPatricio(){
+function clickPatricio() {
     alert("La mayonesa es un instrumento?");
 }
+
+function changeColor( elementHtml, color ) {
+    // alert("Que emoción, voy a cambiar mi color")
+    console.log(elementHtml);
+    console.log(color);
+    
+    elementHtml.style.color = color;
+}
+
+function changeColorWithPromt( elementHtml) {
+    const color = prompt("Dime el color en inglés", "yellow");
+    elementHtml.innerHTML
+    changeColor( elementHtml, color );
+}
+function resetColor(){
+   const refRedColor = document.getElementById("red-color");
+   const refGreenColor = document.getElementById("green-color");
+   const refPurpleColor = document.getElementById("purple-color");
+   const refUserColor = document.getElementById("user-color");
+ 
+  // refRedColor.style.color = "black"
+  changeColor(refGreenColor, "black");
+  changeColor(refRedColor, "black");
+  changeColor(refPurpleColor, "black");
+  changeColor(refUserColor, "black");
+} 
+function changeTextOfUserColor( element, color ) {
+    element.innerHTML = "<h3>Ahora soy <em>" + color + "</em> </h3>";
+}
+function changeName(){
+const userName = prompt("Escribe tu nombre cholo", "machete");
+const refGretting = document.getElementById("gretting");
+refGretting.innerHTML = `Hola ${userName}, la clika te respalda`;
+}
+      
